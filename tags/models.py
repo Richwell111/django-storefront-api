1edeb083ffdeedf38290f21a45d2d5ea3f67e950
Richwell111 <bcict20103@gmail.com>
2026-06-07 20:32:22 +0000

    Refactor admin and models for improved functionality and organization
    - Enhanced ProductAdmin with inventory filtering and custom actions.
    - Added CollectionAdmin and CustomerAdmin with product and order counts.
    - Introduced InventoryFilter for low inventory status in ProductAdmin.
    - Updated models to include validators for price and inventory fields.
    - Added Tag model and TagAdmin for better tag management.
    - Removed unused TaggedItemManager migration and related commented code.
    - Improved queryset annotations for collections and customers.
    - Added new migration for model options and field alterations.

--- tags/models.py
+++ tags/models.py
@@ -1,19 +1,9 @@
-from django.conf import settings
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
-from store.models import Product
 
 
-# class LikedItem(models.Model):
-#     user = models.ForeignKey(settings.AUTH_USER_MODEL,
-#                              on_delete=models.CASCADE)
-#     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-#     object_id = models.PositiveIntegerField()
-#     content_object = GenericForeignKey()
-
-
-class TaggedItemManager(models.Model):
+class TaggedItemManager(models.Manager):
     def get_tags_for(self, obj_type, obj_id):
         content_type = ContentType.objects.get_for_model(obj_type)
 
@@ -28,16 +18,13 @@ class TaggedItemManager(models.Model):
 class Tag(models.Model):
     label = models.CharField(max_length=255)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.label
 
 
 class TaggedItem(models.Model):
     objects = TaggedItemManager()
-    # What tag applied to what object
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
-    # Type (product, video, article)
-    # ID
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()
