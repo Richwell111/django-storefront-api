51ead769756ecea2b147a97373703f3ba58bc1f8
Richwell111 <bcict20103@gmail.com>
2026-06-07 14:15:01 +0000

    Update admin and models for improved display and organization

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Collection, Customer, Order, Product
 
 # Register your models here.
+admin.site.register(Collection)
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(Product)
--- storefront/urls.py
+++ storefront/urls.py
@@ -4,6 +4,9 @@ from django.urls import include, path
 from debug_toolbar.toolbar import debug_toolbar_urls
 from . import views
 
+admin.site.site_header = "Storefront Admin"
+admin.site.site_title = "Storefront Admin Portal"
+admin.site.index_title = "Welcome to Storefront Admin Portal"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.home, name="home"),
