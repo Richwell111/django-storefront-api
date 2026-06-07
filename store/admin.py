64592e7ea980ceb67b5dcfcfee7ec96b7949f534
Richwell111 <bcict20103@gmail.com>
2026-06-07 16:39:10 +0000

    Refactor admin registration for models to improve organization and display

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from .models import Collection, Customer, Order, Product
 
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display =  ['title','unit_price']
+
 # Register your models here.
-admin.site.register(Collection)
-admin.site.register(Customer)
-admin.site.register(Order)
-admin.site.register(Product)
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display=['title','featured_product']
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+      list_display =['first_name','last_name','membership','email']
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id','customer','payment_status','placed_at']
