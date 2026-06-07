ea7d77c07bf5132372d5307c9f39a3b0f352d334
Richwell111 <bcict20103@gmail.com>
2026-06-07 20:34:45 +0000

    Refactor hello template and update view for improved greeting logic

--- playground/views.py
+++ playground/views.py
@@ -1,24 +1,5 @@
 from django.shortcuts import render
-from django.core.exceptions import ObjectDoesNotExist
-from django.db.models.aggregates import Count, Min, Max, Avg, Sum
-from django.db.models import Q,Func,Value, F
-
-from store.models import Collection, Customer, Order, Product
 
 
 def say_hello(request):
-    # try:
-    #     # raise ObjectDoesNotExist()
-    #     # product = Product.objects.get(pk=1)
-    #     # print(product)
-    #     pass
-    # except ObjectDoesNotExist:
-    #     pass
-    # collection = Collection()
-    # collection.title = 'Video Games'
-    # collection.featured_product = Product(pk=1)
-    # collection.save()
-    order=Order()
-    order.customer_id=1
-    order.save()
-    return render(request, 'hello.html', {'name': 'Richman', 'result': list(queryset)})
+    return render(request, 'hello.html', {'name': 'Mosh'})
